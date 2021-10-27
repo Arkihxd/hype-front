c1ca53a352b3ecb828ca676ea2d0f8fe4271e1a0
Mateus Oliveira <mateusoliveiradesouza@hotmail.com>
2021-10-27 14:24:29 -0300

    Start front end project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,137 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="text-banner" class="text-center">
+      <h1>Desafio Hype <span class="fa fa-building"></span></h1>
+      <br />
+      <h6 class="text-secondary">sim zap</h6>
+    </div>
+  </div>
+  <div id="main">
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+          <div class="card">
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label for="nome">Nome</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="nome"
+                    placeholder="Hype Tower"
+                  />
+                </div>
+                 <div class="form-group">
+                  <label for="sigla">Sigla</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="sigla"
+                    placeholder="ZP50"
+                  />
+                </div>
+                 <div class="form-group">
+                  <label for="endereco">Endereço</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="endereco"
+                    placeholder="Rua Hype nº 123"
+                  />
+                </div>
+                 <div class="form-group">
+                  <label for="cidade">Cidade</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="cidade"
+                    placeholder="Curitiba"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="cidade">Cidade</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="cidade"
+                    placeholder="Curitiba"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="estado">Estado</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="estado"
+                    placeholder="PR"
+                    maxlength="2"
+                  />
+                </div>
+                <br/>
+                <button type="submit" class="btn btn-primary">Criar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="card">
+            <div class="card-body">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#text-banner {
+  margin-top: 70px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
 }
 </style>
